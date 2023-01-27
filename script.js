fbaeffdd7abba8e4649ba5ee3284a30610d3fd5b
James <edjcjacobsen@gmail.com>
2023-01-27 02:27:09 -0600

    in showCalendar(): coded getting the first day of the current month and the number of days in the current month.

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 
+showCalendar();
+
+function showCalendar(){
+    let now = new Date();
+    console.log(now.getDate(), now.getDay());
+    firstDay = (now.getDate() % 7) - 1 - now.getDay();
+    console.log(firstDay);
+    let daysInMonth = daysInThisMonth(now);
+    console.log(daysInMonth);
+    console.log(now.getMonth());
+
+    //This function gets the number of days of this month by:
+    //getting the day before next months first day.
+    function daysInThisMonth(n){
+        return new Date(n.getFullYear(), n.getMonth()+1, 0).getDate();
+    }
+}
+
